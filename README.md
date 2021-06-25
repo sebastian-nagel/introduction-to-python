d51c9882ddc7c60ee5041054707d2d085ec04821
Sebastian Nagel <sebastian-nagel@uni-konstanz.de>
2021-06-25 11:03:09 +0200

    Workshop "Introduction to Python" - add README with syllabus - "2. Working with Structured Data"   add notebook/slides, data, requirements

---
+++ README.md
@@ -0,0 +1,71 @@
+Course Materials – Workshop “Introduction to Python”
+====================================================
+
+This repository bundles course materials for a Python Programming
+Workshop organized by the Cluster of Excellence “The Politics of
+Inequality” at the University of Konstanz in cooperation with the
+Zeppelin University Friedrichshafen in summer 2021.
+
+Addressed to scholars in social sciences (in a very broad definition),
+the workshop focuses on using Python in practice. The provided
+hands-on examples will support the participants to use Python for
+their own research interests.
+
+As a short two-day workshop, some (limited) programming skills and a
+first and basic understanding of the Python syntax are expected from
+the participants.
+
+
+# Day One
+
+## 1. Introduction, Warm Up, Set Up
+
+- Python puzzles / recap
+  - data types
+  - control structures
+  - classes and objects
+  - modules
+
+- Python runtime and development environments
+  - Python interpreter
+  - editors, IDEs
+  - Jupyter notebooks, Anaconda
+  - virtual environment, Docker
+
+- last-minute help desk setting up Python work environment
+
+
+## 2. Working with Structured Data
+
+- read data from local files
+- CSV and JSON
+- elementary data analysis: Pandas and data frames
+- plotting basics
+
+Notebook: [working with structured data – the “Tree Cadastre of the City of Konstanz”](2_structured_data_tree_cadastre_konstanz.ipynb)
+
+
+## 3. The Twitter API
+
+- what is an API?
+- get access to the API
+- use a client: DocNow/twarc 
+- tweets, user timelines, followers, trends
+- text statistics, language, sentiment
+
+
+# Day Two
+
+## 4. Web Scraping
+
+- HTTP requests
+- HTML, XML, DOM, CSS selectors, XPath
+- browser automation
+- cleanse and export extracted data
+
+
+## 5. Text Processing and Machine Learning
+
+- pre-processing and tokenization (splitting text into words)
+- n-grams, vectorization and word embeddings
+- train and evaluate a text classifier
---
+++ scripts/trees_wikispecies.py
@@ -0,0 +1,71 @@
+import json
+import os.path
+
+import pandas as pd
+import requests
+
+output = 'data/trees-wikispecies.json'
+
+tree_cadastre_file = './data/KN_Baumkataster_2020.csv'
+df = pd.read_csv(tree_cadastre_file)
+
+N = 500
+top_trees = df['Name_lat'].value_counts().head(500).to_frame()
+
+
+# some tree names are too specific or are listed by synonyms
+tree_synonyms = {
+    'Platanus x acerifolia': 'Platanus × hispanica',
+    'Thuja orientalis': 'Platycladus orientalis',
+}
+
+query_params = {
+    'action': 'query',
+    'format': 'json',
+    'prop': 'iwlinks|langlinks|description',
+    'lllimit': 300,
+    'llprop': 'url|langname'
+}
+
+trees_wikispecies = {}
+if os.path.exists(output):
+    # update
+    trees_wikispecies = json.load(open(output))
+
+for tree in top_trees.index.to_list():
+    q = tree
+    if q in tree_synonyms:
+        q = tree_synonyms[q]
+    while True:
+        if q in trees_wikispecies:
+            print("Already done:", q)
+            break
+        print("Querying API for", q)
+        query_params['titles'] = q.replace(' ', '_')
+        response = requests.get('https://species.wikimedia.org/w/api.php',
+                                params=query_params)
+        data = json.loads(response.text)
+        if "-1" in data["query"]["pages"]:
+            print("Nothing found for", q)
+            if "'" in q:
+                # try without the variety in single quotes
+                #  Fagus sylvatica 'Asplenifolia'
+                q = q[:q.index("'")].rstrip()
+                continue
+            if " x " in q:
+                # try without the " x "
+                #  Prunus x serrulata
+                #  => Prunus serrulata
+                q = q.replace(" x ", " ")
+                continue
+            if " ssp. " in q:
+                # try without the supspecies
+                q = q[:q.index(" ssp. ")].rstrip()
+                continue
+        else:
+            trees_wikispecies[tree] = data
+            trees_wikispecies[q] = trees_wikispecies[tree]
+        break
+
+with open('data/trees-wikispecies.json', 'w') as fp:
+    json.dump(trees_wikispecies, fp)
